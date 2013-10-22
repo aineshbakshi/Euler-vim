c1cd8adc957163ce0c00f3a945e74dcd89eb2422
cyborgqs <aineshbakshi@gmail.com>
2013-10-21 23:26:47 -0400

    Update prob10.java

--- prob10.java
+++ prob10.java
@@ -1,26 +1,31 @@
-public class prob10{
-    public static boolean isPrime(long n )
-    {
-        for (long i = 2; i< n; i++)
-        {
-            if ( n %i == 0)
-                return false;
-        }
-        return true;
-    }
-
-    public static void main(String args [] )
-    {
-        long sum = 0;
-        for ( int i =2; i < 2000000; i++)
-        {
-            if ( isPrime(i))
-            {
-                sum = sum + i;
-            }
-        }
-
-        System.out.println(sum);
-    }
-}
-
+ import java.util.ArrayList;
+ 
+ public class prob10{
+     public static ArrayList<Integer> primes = new ArrayList <Integer> ();
+ 
+     public static boolean isPrime(long i)
+     {
+         for(long j = 0; j< primes.size(); j++)
+         {
+             if ( i % primes.get((int)j) == 0)
+                 return false;
+         }
+         return true;
+     }
+ 
+     public static void main(String [] args)
+     {
+         primes.add(2);
+ 
+         long sum = 2;
+         for(int i =3; i<2000000; i = i +2){
+ 
+             if( isPrime(i))
+             {
+                 sum = sum +i;
+                 primes.add(i);
+             }
+         }
+         System.out.println(sum);
+     }
+ }
